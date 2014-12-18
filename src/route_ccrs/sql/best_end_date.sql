a5589a73b1cefce7ffa0a737bd2895c396aa2039
Adam Harper <github@adam-harper.com>
2014-12-18 14:33:44 +0000

    _Correctly_ take pre-ccr-buffer into account

--- src/route_ccrs/sql/best_end_date.sql
+++ src/route_ccrs/sql/best_end_date.sql
@@ -2,12 +2,19 @@ select
   min(
     ifsapp.work_time_calendar_api.get_end_date(
       wc.calendar_id,
-      trunc(sysdate),
-      (
-        :pre_ccr_buffer +
-        (:total_touch_time / 60 / wc.average_capacity) +
-        :post_ccr_buffer
-      )
+      ifsapp.work_time_calendar_api.get_end_time(
+        wc.calendar_id,
+        greatest(
+          fc.start_work_day,
+          ifsapp.work_time_calendar_api.get_end_date(
+            wc.calendar_id,
+            trunc(sysdate),
+            :pre_ccr_buffer
+          )
+        ),
+        :total_touch_time
+      ),
+      :post_ccr_buffer
     )
   ) best_end_date
 from finiteload.free_capacity fc
@@ -16,13 +23,25 @@ join ifsapp.work_center wc
   and fc.work_center_no = wc.work_center_no
 where fc.contract = :contract
   and fc.work_center_no = :work_center_no
+  and fc.finish_work_day >=
+    ifsapp.work_time_calendar_api.get_end_date(
+      wc.calendar_id,
+      trunc(sysdate),
+      :pre_ccr_buffer
+    )
+  and fc.start_work_day <=
+    ifsapp.work_time_calendar_api.get_start_time(
+      wc.calendar_id,
+      fc.finish_work_day,
+      :total_touch_time
+    )
   and ifsapp.work_time_calendar_api.get_end_date(
       wc.calendar_id,
       trunc(sysdate),
       :pre_ccr_buffer
-    ) >= fc.start_work_day
-  and ifsapp.work_time_calendar_api.get_end_date(
-    wc.calendar_id,
-    trunc(sysdate),
-    :pre_ccr_buffer + (:total_touch_time / 60 / wc.average_capacity)
-  ) <= fc.finish_work_day
+    ) <=
+    ifsapp.work_time_calendar_api.get_start_time(
+      wc.calendar_id,
+      fc.finish_work_day,
+      :total_touch_time
+    )
