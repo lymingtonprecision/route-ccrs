b3d5a81e1f7f4df523133423f340af371ce35f95
Adam Harper <github@adam-harper.com>
2014-12-18 12:18:18 +0000

    Take pre-ccr-buffer into account when calculating best end date

--- src/route_ccrs/sql/best_end_date.sql
+++ src/route_ccrs/sql/best_end_date.sql
@@ -2,8 +2,12 @@ select
   min(
     ifsapp.work_time_calendar_api.get_end_date(
       wc.calendar_id,
-      greatest(fc.start_work_day, trunc(sysdate)),
-      (:total_touch_time / 60 / wc.average_capacity) + :post_ccr_buffer
+      trunc(sysdate),
+      (
+        :pre_ccr_buffer +
+        (:total_touch_time / 60 / wc.average_capacity) +
+        :post_ccr_buffer
+      )
     )
   ) best_end_date
 from finiteload.free_capacity fc
@@ -12,8 +16,13 @@ join ifsapp.work_center wc
   and fc.work_center_no = wc.work_center_no
 where fc.contract = :contract
   and fc.work_center_no = :work_center_no
-  and (
-    fc.start_work_day >= trunc(sysdate) or
-    (fc.start_work_day < trunc(sysdate) and fc.finish_work_day >= trunc(sysdate))
-  )
-  and fc.capacity_available >= :total_touch_time
+  and ifsapp.work_time_calendar_api.get_end_date(
+      wc.calendar_id,
+      trunc(sysdate),
+      :pre_ccr_buffer
+    ) >= fc.start_work_day
+  and ifsapp.work_time_calendar_api.get_end_date(
+    wc.calendar_id,
+    trunc(sysdate),
+    :pre_ccr_buffer + (:total_touch_time / 60 / wc.average_capacity)
+  ) <= fc.finish_work_day
