d52b297c8d1856091c71f6b1eeb3451143661774
Adam Harper <github@adam-harper.com>
2015-02-05 10:06:54 +0000

    Release 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject route-ccrs "1.0.1"
+(defproject route-ccrs "1.1.0"
   :description "Calculates the current CCR for active routings in IFS"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
