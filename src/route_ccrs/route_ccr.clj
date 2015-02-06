94295f2e3b22c39ac9d28dbc0e23d056a3b71d5b
Adam Harper <github@adam-harper.com>
2015-02-06 16:09:03 +0000

    Apply a tolerance when checking for calculated field differences
    Also helps alleviate false positive comparisons due to type differences

--- src/route_ccrs/route_ccr.clj
+++ src/route_ccrs/route_ccr.clj
@@ -6,6 +6,7 @@
             [yesql.core :refer [defquery]]))
 
 (def buffer-factor 1.5)
+(def buffer-tolerance 0.041M) ; about an hour
 
 (defquery best-end-date "route_ccrs/sql/best_end_date.sql")
 (defquery best-unconstrained-end-date "route_ccrs/sql/best_unconstrained_end_date.sql")
@@ -86,8 +87,34 @@
            first)
       (create-unconstrained-ccr-entry db r))))
 
+(defn- split-keys
+  ([map] map)
+  ([map key]
+   [(dissoc map key) (select-keys map [key])])
+  ([map key & ks]
+   (let [ks (conj ks key)]
+     [(apply dissoc map ks)
+      (select-keys map ks)])))
+
+(defn- outside-tolerance? [k x y]
+  (let [x (get x k)
+        y (get y k)]
+    (if (and x y)
+      (-> (- x y) bigdec .abs (> buffer-tolerance))
+      true)))
+
+(defn diff-ccrs [x y]
+  (reduce
+    (fn [[n o s] k]
+      (if (and (contains? n k)
+               (outside-tolerance? k n o))
+        [n o s]
+        [(dissoc n k) (dissoc o k) (merge s (select-keys o [k]))]))
+    (diff x y)
+    [:total_touch_time :pre_ccr_buffer :post_ccr_buffer]))
+
 (defn ccr-entry-updates [id current-ccr new-ccr]
-  (let [[old-values new-values no-change] (diff current-ccr new-ccr)
+  (let [[old-values new-values no-change] (diff-ccrs current-ccr new-ccr)
         updated-ccr (merge id no-change new-values)
         ts (java.sql.Timestamp. (.getTime (java.util.Date.)))]
     (if (seq new-values)
