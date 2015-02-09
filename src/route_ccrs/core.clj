8b90cac250b811f524db509256a06bffb8a5c276
Adam Harper <github@adam-harper.com>
2015-02-09 15:37:32 +0000

    Use async channels to produce route updates in parallel
    Offers a reasonable speed up when there are few DB updates

--- src/route_ccrs/core.clj
+++ src/route_ccrs/core.clj
@@ -1,5 +1,6 @@
 (ns route-ccrs.core
   (:require [clojure.tools.logging :as log]
+            [clojure.core.async :as async]
             [route-ccrs.active-routes :as ar]
             [route-ccrs.route-ccr :as ccr]))
 
@@ -10,22 +11,51 @@
          ~'c {:connection ~'db}]
      ~@body))
 
-(defn- route-update [sys [route-id operations]]
+(defn- route-update
+  "Returns a record of any required database updates to reflect changes
+  in the routes current CCR details, or `nil` if the CCR hasn't changed"
+  [sys [route-id operations]]
   (with-db-connection sys
     (ccr/ccr-entry-updates
       route-id
       (ccr/get-last-known-ccr route-id c)
       (ccr/select-current-ccr db operations))))
 
-(defn- reduce-to-updates [sys routes]
-  (reduce
-    (fn [u route]
-      (let [ru (route-update sys route)]
-        (if (seq ru)
-          (conj u ru)
-          u)))
+(defn- route-processor
+  "Returns a `go-loop` that takes route entries from the `<routes`
+  channel, and puts details of any required database updates to the
+  `>updates` channel.
+
+  The `go-loop` will terminate when no more entries can be retrieved
+  from the `>routes` channel."
+  [sys <routes >updates]
+  (async/go-loop
     []
-    routes))
+    (let [r (async/<! <routes)]
+      (if (nil? r)
+        :done
+        (let [u (route-update sys r)]
+          (if (seq u) (async/>! >updates u))
+          (recur))))))
+
+(defn- reduce-routes-to-updates
+  "Takes a collection of routes and returns a collection of required CCR
+  database updates."
+  [sys routes]
+  (let [<routes (async/to-chan routes)
+        >updates (async/chan 10)
+        <updates (async/into [] >updates)
+        !workers (async/merge
+                   (map
+                     (fn [n] (route-processor sys <routes >updates))
+                     (range 10)))]
+    (loop []
+      (let [w (async/<!! !workers)]
+        (if (nil? w)
+          (do
+            (async/close! >updates)
+            (async/<!! <updates))
+          (recur))))))
 
 (defn- apply-updates! [sys updates]
   (with-db-connection sys
@@ -37,10 +67,10 @@
       {}
       updates)))
 
-(defn calculate-and-record-ccrs!  [sys]
+(defn calculate-and-record-ccrs! [sys]
   (with-db-connection sys
     (log/info "CCR calculation started")
     (->> (into [] ar/transduce-routes (ar/active-routes {} c))
-         (reduce-to-updates sys)
+         (reduce-routes-to-updates sys)
          (apply-updates! sys)
          (log/info "calculation complete, updated:"))))
