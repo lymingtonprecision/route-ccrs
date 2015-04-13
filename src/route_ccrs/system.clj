f1ba2d9f6785e80b8cff1330b16d60e4e8de9c42
Adam Harper <github@adam-harper.com>
2015-04-13 12:19:12 +0100

    Add the IFS part store and date calculators to the system

--- src/route_ccrs/system.clj
+++ src/route_ccrs/system.clj
@@ -2,14 +2,18 @@
   (:require [com.stuartsierra.component :as component]
             [environ.core :refer (env)]
             [route-ccrs.logging :as log]
-            [route-ccrs.database :as db]))
+            [route-ccrs.database :as db]
+            [route-ccrs.parts :refer [ifs-part-store]]
+            [route-ccrs.best-end-dates.ifs :refer [ifs-date-calculator]]))
 
 (defn system
   ([] (system env))
   ([env]
    (component/system-map
      :env env
-     :db (db/database))))
+     :db (db/database)
+     :part-store (ifs-part-store)
+     :date-calculator (ifs-date-calculator))))
 
 (defn start [s]
   (log/start!)
