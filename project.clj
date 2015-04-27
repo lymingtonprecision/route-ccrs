91d02556662d3eb21ba3cfd8a75070cde0c1cfb8
Adam Harper <github@adam-harper.com>
2015-04-27 15:31:41 +0100

    Bump ClojureScript version 0.0-3196 to 0.0-3211

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/clojurescript "0.0-3196"]
+                 [org.clojure/clojurescript "0.0-3211"]
                  [com.stuartsierra/component "0.2.3"]
                  [clj-time "0.9.0"]
                  [com.andrewmcveigh/cljs-time "0.3.4"]
